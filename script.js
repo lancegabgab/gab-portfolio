b2741458760f2a295563aedff06f1e9a7464b44c
Lance Gabriel Cruz <nerd.dev2@victoriacourt.com>
2025-07-13 00:13:27 +0800

    removed background image on landing, animated title

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+$(function () {
+  var texts = [
+    "FULL STACK DEVELOPER",
+    "FRONT END DEVELOPER",
+    "BACKEND DEVELOPER"
+  ];
+
+  var count = 0;
+  var index = 0;
+  var currentText = '';
+  var isDeleting = false;
+  var typingSpeed = 120;
+  var erasingSpeed = 50;
+  var delayBetweenWords = 2000;
+
+  function typeEffect() {
+    currentText = texts[count];
+    var displayed = isDeleting
+      ? currentText.substring(0, index--)
+      : currentText.substring(0, index++);
+
+    $('#animated-text').text(displayed || '\u00A0');
+
+    if (!isDeleting && index === currentText.length + 1) {
+      isDeleting = true;
+      setTimeout(typeEffect, delayBetweenWords);
+    } else if (isDeleting && index === 0) {
+      isDeleting = false;
+      count = (count + 1) % texts.length;
+      setTimeout(typeEffect, typingSpeed);
+    } else {
+      setTimeout(typeEffect, isDeleting ? erasingSpeed : typingSpeed);
+    }
+  }
+
+  typeEffect();
+});
 $(document).ready(function () {
     // Mouseenter event
     $(".card").mouseenter(function () {
